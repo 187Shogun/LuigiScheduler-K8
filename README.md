d7e3a2dc55f714bc922d3f8f6fc7a79ae51be7e2
187-Shogun <187shogun@gmail.com>
2022-09-05 21:05:51 -0600

    Updated docs and deployment script.

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ dashboard in a K8 service.
 The K8 pod running the scheduler runs on a Docker image with GCP
 components as base and it is configured to earn the rights of a 
 given service account if you bind the K8 and IAM service accounts.
+
+### Instructions: 
+
+1. Run `gcloud init` and authenticate into a GCP cloud account.
+2. Create a service account and a K8 cluster in GCP. 
+3. Run `run-deployment.ps1` to authenticate into the K8 server, bind a service account and deploy assets. 
---
+++ run-deployment.ps1
@@ -0,0 +1,23 @@
+#Define parameters:
+$PROJECT_ID="cmg-data-hub-dev-001"
+$K8_SA="gke-worker-sa"
+$IAM_SA="aldah-worker-dev"
+$CLUSTER_NAME="autopilot-cluster-1"
+$NAMESPACE="default"
+$REGION="us-central1"
+
+# Authenticate into the K8 cluster:
+echo "Authenticating into K8 cluster..."
+gcloud container clusters get-credentials $CLUSTER_NAME --region $REGION --project $PROJECT_ID
+
+# Create a K8 service account and bind it to a GCPservice account:
+echo "Binding service accounts..."
+kubectl create serviceaccount $K8_SA --namespace $NAMESPACE
+gcloud iam service-accounts add-iam-policy-binding $IAM_SA@$PROJECT_ID.iam.gserviceaccount.com --role roles/iam.workloadIdentityUser --member "serviceAccount:$PROJECT_ID.svc.id.goog[default/$K8_SA]"
+kubectl annotate serviceaccount $K8_SA --namespace $NAMESPACE iam.gke.io/gcp-service-account=$IAM_SA@$PROJECT_ID.iam.gserviceaccount.com
+
+# Deploy assets to K8 cluster:
+echo "Deploying assets..."
+kubectl apply -f pvc.yaml
+kubectl apply -f deployment.yaml
+echo "Deployment successful!"
